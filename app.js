125f1bf13586a6a1b64db6df59a3f5a789c99c04
Gianna-mancini <giannamancini41@gmail.com>
2026-03-29 20:16:52 +0000

    Create Domain Name Generator

---
+++ app.js
@@ -0,0 +1,14 @@
+const p = ['the', 'our'];
+const a = ['great', 'big'];
+const n = ['jogger', 'racoon'];
+
+function generateDomainName(pronoun, adj, noun) {
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+            }
+        }
+    }
+} 
+generateDomainName(p, a, n);
